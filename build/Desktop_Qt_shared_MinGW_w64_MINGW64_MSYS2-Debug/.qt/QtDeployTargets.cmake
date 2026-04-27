a4b4cb79ab92e48ec57147be21b626ad49b84db3
Robert <rob.ibr0407@gmail.com>
2026-04-27 22:50:32 +0300

    Remove build folder from repository

--- build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_EP_FILE C:/Users/Zenbook/Desktop/EP/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/EP.exe)
-set(__QT_DEPLOY_TARGET_EP_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_EP_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
--- build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/CMakeFiles/EP.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for EP.
-# This may be replaced when dependencies are built.
